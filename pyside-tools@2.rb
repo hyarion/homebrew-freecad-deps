773efcfda974891ce2eff526fce2b98f532b1970
Benjamin Nauck <benjamin@nauck.se>
2021-03-21 20:34:08 +0100

    Remove bottles from pyside-tools

--- pyside-tools@2.rb
+++ pyside-tools@2.rb
@@ -9,13 +9,6 @@ class PysideToolsAT2 < Formula
   depends_on "python@3.9" => :build
   depends_on "pyside@2"
 
-  bottle do
-    root_url "https://dl.bintray.com/vejmarie/freecad"
-    cellar :any
-    sha256 "d3ab67c4bd9e47f8505b36445c496fca3109aab1a4ad59a0c370734c9001c3c3" => :big_sur
-    sha256 "313cdb6754ad9f62abd03e8bfcc9f270bc308a5405fe91a56659d26d420db287" => :catalina
-  end
-
   def install
       mkdir "macbuild3.9" do
         args = std_cmake_args
